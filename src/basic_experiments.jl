cb63d71d25aaf7bad8d2ec3aee636c7b694d6c40
mgoloshchapov <m.goloshchapov@yandex.ru>
2023-11-09 11:32:11 +0300

    Created basic_experiments for further development

---
+++ src/basic_experiments.jl
@@ -0,0 +1,9 @@
+"""
+trap_params: [U0(μK), w0(μm), z0(μm)]
+atom_params: [m(a.u.), T(μK)], T is used as initial guess for temperature
+tspan      : time from experiment(in μs)
+probability: probability to recapture atom from experiment
+"""
+function release_recapture(trap_params, atom_params, tspan, probability)
+    #...
+end;
